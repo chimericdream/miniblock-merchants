74f4eb0d72d5b78047e14258c2f944c7a1239b6b
Bill Parrott <bill@chimericdream.com>
2022-11-09 23:50:56 -0600

    feat(compat): started work on YUNG's compat

--- src/main/java/com/chimericdream/blocktraders/loot/BTLootTables.java
+++ src/main/java/com/chimericdream/blocktraders/loot/BTLootTables.java
@@ -23,6 +23,43 @@ public class BTLootTables {
     private static final Identifier SPRUCE_LEAVES_TABLE_ID = Blocks.SPRUCE_LEAVES.getLootTableId();
     private static final Identifier WHEAT_TABLE_ID = Blocks.WHEAT.getLootTableId();
 
+    private static final Identifier YUNGS_EXTRAS_DESERT_CHILLZONE_ID = new Identifier("yungsextras", "desert/chillzone");
+    private static final Identifier YUNGS_EXTRAS_DESERT_RUINS_ID = new Identifier("yungsextras", "desert/ruins");
+
+    private static final Identifier YUNGS_BDT_FOOD_STORAGE_ID = new Identifier("betterdeserttemples", "chests/food_storage");
+    private static final Identifier YUNGS_BDT_LAB_ID = new Identifier("betterdeserttemples", "chests/lab");
+    private static final Identifier YUNGS_BDT_LIBRARY_ID = new Identifier("betterdeserttemples", "chests/library");
+    private static final Identifier YUNGS_BDT_PHARAOH_HIDDEN_ID = new Identifier("betterdeserttemples", "chests/pharaoh_hidden");
+    private static final Identifier YUNGS_BDT_POT_ID = new Identifier("betterdeserttemples", "chests/pot");
+    private static final Identifier YUNGS_BDT_STATUE_ID = new Identifier("betterdeserttemples", "chests/statue");
+    private static final Identifier YUNGS_BDT_STORAGE_ID = new Identifier("betterdeserttemples", "chests/storage");
+    private static final Identifier YUNGS_BDT_TOMB_PHARAOH_ID = new Identifier("betterdeserttemples", "chests/tomb_pharaoh");
+    private static final Identifier YUNGS_BDT_TOMB_ID = new Identifier("betterdeserttemples", "chests/tomb");
+    private static final Identifier YUNGS_BDT_WARDROBE_ID = new Identifier("betterdeserttemples", "chests/wardrobe");
+
+    private static final Identifier YUNGS_BD_SKELETON_COMMON_ID = new Identifier("betterdungeons", "skeleton_dungeon/chests/common");
+    private static final Identifier YUNGS_BD_SKELETON_MIDDLE_ID = new Identifier("betterdungeons", "skeleton_dungeon/chests/middle");
+    private static final Identifier YUNGS_BD_SMALL_LOOT_ID = new Identifier("betterdungeons", "small_dungeon/chests/loot_piles");
+    private static final Identifier YUNGS_BD_SPIDER_EGG_ROOM_ID = new Identifier("betterdungeons", "spider_dungeon/chests/egg_room");
+    private static final Identifier YUNGS_BD_ZOMBIE_COMMON_ID = new Identifier("betterdungeons", "zombie_dungeon/chests/common");
+    private static final Identifier YUNGS_BD_ZOMBIE_SPECIAL_ID = new Identifier("betterdungeons", "zombie_dungeon/chests/special");
+    private static final Identifier YUNGS_BD_ZOMBIE_TOMBSTONE_ID = new Identifier("betterdungeons", "zombie_dungeon/chests/tombstone");
+
+    private static final Identifier YUNGS_BOM_UPPER_SIDE_ID = new Identifier("betteroceanmonuments", "chests/upper_side_chamber");
+
+    private static final Identifier YUNGS_BSH_ARMOURY_ID = new Identifier("betterstrongholds", "chests/armoury");
+    private static final Identifier YUNGS_BSH_CMD_YUNG_ID = new Identifier("betterstrongholds", "chests/cmd_yung");
+    private static final Identifier YUNGS_BSH_COMMON_ID = new Identifier("betterstrongholds", "chests/common");
+    private static final Identifier YUNGS_BSH_CRYPT_ID = new Identifier("betterstrongholds", "chests/crypt");
+    private static final Identifier YUNGS_BSH_GRAND_LIBRARY_ID = new Identifier("betterstrongholds", "chests/grand_library");
+    private static final Identifier YUNGS_BSH_LIBRARY_MD_ID = new Identifier("betterstrongholds", "chests/library_md");
+    private static final Identifier YUNGS_BSH_MESS_ID = new Identifier("betterstrongholds", "chests/mess");
+    private static final Identifier YUNGS_BSH_PRISON_LG_ID = new Identifier("betterstrongholds", "chests/prison_lg");
+    private static final Identifier YUNGS_BSH_TRAP_ID = new Identifier("betterstrongholds", "chests/trap");
+    private static final Identifier YUNGS_BSH_TREASURE_ID = new Identifier("betterstrongholds", "chests/treasure");
+
+    private static final Identifier YUNGS_BWH_HUT_0_ID = new Identifier("betterwitchhuts", "chests/hut_0");
+
     private static LootPool.Builder makeBuilder(Item item, int chance) {
         LootPool.Builder builder = LootPool.builder()
             .with(ItemEntry.builder(item));
@@ -32,6 +69,26 @@ public class BTLootTables {
         }
 
         return builder.with(ItemEntry.builder(Items.AIR).weight(chance - 1));
+    }
+
+    private static void checkYungsLootTables(Identifier id, List<LootPool.Builder> poolBuilders, BlockTradersConfig config) {
+        if (
+            YUNGS_BDT_POT_ID.equals(id) ||
+                YUNGS_BDT_STORAGE_ID.equals(id) ||
+                YUNGS_EXTRAS_DESERT_CHILLZONE_ID.equals(id) ||
+                YUNGS_EXTRAS_DESERT_RUINS_ID.equals(id)
+        ) {
+            poolBuilders.add(makeBuilder(ModItems.BUDDING_CACTUS, config.buddingCactusChance));
+        }
+
+        if (
+            YUNGS_BDT_LIBRARY_ID.equals(id) ||
+                YUNGS_BSH_GRAND_LIBRARY_ID.equals(id) ||
+                YUNGS_BSH_LIBRARY_MD_ID.equals(id)
+        ) {
+            poolBuilders.add(makeBuilder(ModItems.ENDLESS_BOOKSHELF, config.endlessBookshelfChance));
+            poolBuilders.add(makeBuilder(ModItems.BOOK_OF_RITUALS, config.bookOfRitualsChance));
+        }
     }
 
     private static void checkVanillaLootTables(Identifier id, List<LootPool.Builder> poolBuilders, BlockTradersConfig config) {
@@ -152,6 +209,7 @@ public class BTLootTables {
             List<LootPool.Builder> poolBuilders = new ArrayList<>();
 
             checkVanillaLootTables(id, poolBuilders, config);
+            checkYungsLootTables(id, poolBuilders, config);
 
             for (LootPool.Builder builder : poolBuilders) {
                 tableBuilder.pool(builder);
