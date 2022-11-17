ece4a43ed7dea440ea1b3399a2f322abb1839dec
Bill Parrott <bill@chimericdream.com>
2022-11-17 01:32:53 -0600

    feat(advancements): add new stat and advancements

--- src/main/java/com/chimericdream/miniblockmerchants/mixin/MMMerchantEntityMixin.java
+++ src/main/java/com/chimericdream/miniblockmerchants/mixin/MMMerchantEntityMixin.java
@@ -3,8 +3,10 @@ package com.chimericdream.miniblockmerchants.mixin;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -15,6 +17,8 @@ abstract public class MMMerchantEntityMixin extends MobEntity {
 
     @Shadow
     abstract protected void produceParticles(ParticleEffect parameters);
+
+    @Shadow @Nullable public abstract PlayerEntity getCustomer();
 
     protected MMMerchantEntityMixin(EntityType<? extends MobEntity> entityType, World world) {
         super(entityType, world);
